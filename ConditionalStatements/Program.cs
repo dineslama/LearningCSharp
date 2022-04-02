6f600925eef5e1963baa34268c04bdfcf2d33138
Dinesh Lama <dineslama@outlook.com>
2022-04-02 21:41:25 +0545

    practice continued

---
+++ ConditionalStatements/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // If Condition
+            Console.WriteLine("Enter a number from 1 to 7");
+            var day = Convert.ToInt16(Console.ReadLine());
+
+            if (day == 1)
+                Console.WriteLine("Sunday");
+            else if (day == 2)
+                Console.WriteLine("Monday");
+            else if (day == 3)
+                Console.WriteLine("Tuesday");
+            else if (day == 4)
+                Console.WriteLine("Wednesday");
+            else if (day == 5)
+                Console.WriteLine("Thursday");
+            else if (day == 6)
+                Console.WriteLine("Friday");
+            else if (day == 7)
+                Console.WriteLine("Saturday");
+            else
+                Console.WriteLine("Invalid Number Entered.");
+
+            //switch condition
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                default:
+
+                    Console.WriteLine("Invalid Number Entered.");
+                    break;
+            }
+
+            //ternary operator
+            //ternary operator for odd and even number
+            Console.WriteLine("Enter a number:");
+            var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number % 2 == 0 ? "It is a even number" : "It is a Odd number");
+
+            var IsEven = number % 2 == 0 ? true : false;
+            //if else to print output
+            if (IsEven == true)
+                Console.WriteLine("The number is even number");
+            else
+                Console.WriteLine("The number is odd number");
+            //ternary operator to print output
+            Console.WriteLine(IsEven == true ? "The number is even number" : "The number is odd number");
+        }
+    }
+}
---
+++ DataTypesAndCasting/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace DataTypesAndCasting
+
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //implicit casting
+            int a = 'A';
+            Console.WriteLine(a);
+            int i = 1245;
+            long c = i;
+            long l = 'B';
+            Console.WriteLine(l);
+            double d = 12.25f;
+            Console.WriteLine(d);
+            //explicit casting
+            double num1 = 75.25;
+            int num2 = (int)num1;
+            char char1 = (char)num1;
+
+            int num3 = 100;
+            string str = num3.ToString();
+            int num4 = int.Parse("123");
+
+            byte thisIsByte = 1;
+            Console.WriteLine("byte: " + thisIsByte);
+            sbyte thisIsSignedByte = -1;
+            Console.WriteLine("sbyte: " + thisIsSignedByte);
+            short thisIsShort = -2;
+            Console.WriteLine("short: " + thisIsShort);
+            ushort thisIsUnsignedShort = 2;
+            Console.WriteLine("ushort: " + thisIsUnsignedShort);
+            int thisIsInt = -4;
+            Console.WriteLine("int: " + thisIsInt);
+            uint thisIsUnsignedInt = 4;
+            Console.WriteLine("uint: " + thisIsUnsignedInt);
+            float thisIsFloat = 8;
+            Console.WriteLine("float: " + thisIsFloat);
+            double thisIsDouble = 10.5;
+            Console.WriteLine("double: " + thisIsDouble);
+            long thisIsLong = -123456;
+            Console.WriteLine("long: " + thisIsLong);
+            ulong thisIsUnsignedlong = 123456;
+            Console.WriteLine("ulong: " + thisIsUnsignedlong);
+            bool thisIsBool = true;
+            Console.WriteLine("bool: " + thisIsBool);
+            char thisIsChar = 'c';
+            Console.WriteLine("char: " + thisIsChar);
+            string thisIsString = "string";
+            Console.WriteLine("string: " + thisIsString);
+            object thisIsObject = new object();
+            Console.WriteLine("object: " + thisIsObject);
+            DateTime thisIsDateTime = new DateTime();
+            Console.WriteLine("DateTime: " + thisIsDateTime);
+            decimal thisIsDecimal = 1234567890;
+            Console.WriteLine("decimal: " + thisIsDecimal);
+
+            //implicit conversion
+            thisIsShort = thisIsByte;
+            thisIsShort = thisIsSignedByte;
+            thisIsUnsignedShort = thisIsByte;
+            thisIsUnsignedShort = thisIsChar;
+            thisIsInt = thisIsByte;
+            thisIsInt = thisIsSignedByte;
+            thisIsInt = thisIsChar;
+            thisIsInt = thisIsShort;
+            thisIsInt = thisIsUnsignedShort;
+            thisIsUnsignedInt = thisIsByte;
+            thisIsUnsignedInt = thisIsChar;
+            thisIsUnsignedInt = thisIsUnsignedShort;
+            thisIsFloat = thisIsByte;
+            thisIsFloat = thisIsSignedByte;
+            thisIsFloat = thisIsShort;
+            thisIsFloat = thisIsUnsignedShort;
+            thisIsFloat = thisIsChar;
+            thisIsFloat = thisIsInt;
+            thisIsFloat = thisIsUnsignedInt;
+            thisIsDouble = thisIsByte;
+            thisIsDouble = thisIsSignedByte;
+            thisIsDouble = thisIsShort;
+            thisIsDouble = thisIsUnsignedShort;
+            thisIsDouble = thisIsChar;
+            thisIsDouble = thisIsInt;
+            thisIsDouble = thisIsUnsignedInt;
+            thisIsDouble = thisIsFloat;
+            thisIsLong = thisIsByte;
+            thisIsLong = thisIsSignedByte;
+            thisIsLong = thisIsShort;
+            thisIsLong = thisIsUnsignedShort;
+            thisIsLong = thisIsChar;
+            thisIsLong = thisIsInt;
+            thisIsLong = thisIsUnsignedInt;
+            thisIsUnsignedlong = thisIsByte;
+            thisIsUnsignedlong = thisIsUnsignedShort;
+            thisIsUnsignedlong = thisIsChar;
+            thisIsUnsignedlong = thisIsUnsignedInt;
+
+            //explicit conversion
+            thisIsFloat = (float)thisIsDouble;
+            Console.WriteLine("float: " + thisIsFloat);
+
+            string str1 = i.ToString();
+            object o = str1;
+            o = i;
+            //o = f;
+            o = l;
+        }
+    }
+
+    namespace test
+    {
+        public class Program
+        {
+        }
+    }
+}
